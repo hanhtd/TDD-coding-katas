301b53aaf22bd73f83a2095c96a3179f7c7436bd
Hanh Truong <hanhtrd@yahoo.com>
2022-03-03 18:17:30 +0700

    String Calculator TDD kata + Add failed test for step 4

--- src/test/java/org/hanhtrd/stringcalculator/StringCalculatorUnitTest.java
+++ src/test/java/org/hanhtrd/stringcalculator/StringCalculatorUnitTest.java
@@ -6,7 +6,6 @@ import junitparams.naming.TestCaseName;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.Collection;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -16,34 +15,32 @@ public class StringCalculatorUnitTest {
 
     @Test
     @Parameters(method = "testData")
-    @TestCaseName("add({0})={1}")
+    @TestCaseName("add('{0}')={1}")
     public void test_that_string_calculator_return_expected_total_value(String numbersAsString, int expectedSum) {
         StringCalculator calculator = new StringCalculator();
 
         assertThat(calculator.add(numbersAsString)).isEqualTo(expectedSum);
     }
 
-    private Collection<Object[]> testData() {
-        return List.of(
-            new Object[][]{
-                { "", 0 },
+    private Object testData() {
+        return new Object[][]{
+            { "", 0 },
 
-                { "1", 1 },
+            { "1", 1 },
 
-                { "10", 10 },
-                { "21", 21 },
+            { "10", 10 },
+            { "21", 21 },
 
-                { "1,2", 3 },
-                { "6,4", 10 },
-                { "1,4,5", 10 },
+            { "1,2", 3 },
+            { "6,4", 10 },
+            { "1,4,5", 10 },
 
-                { "1\n4\n5", 10 },
-                { "1\n4\n5\n7", 17 },
+            { "1\n4\n5", 10 },
+            { "1\n4\n5\n7", 17 },
 
-                { "1,4\n5", 10 },
-                { "1\n4,5", 10 },
-            }
-        );
+            { "1,4\n5", 10 },
+            { "1\n4,5", 10 },
+        };
     }
 
     @Test
@@ -66,6 +63,22 @@ public class StringCalculatorUnitTest {
             "1,,",
             "1\n\n\n"
         );
+    }
+
+    @Test
+    @Parameters(method = "testDataWithDelimiterDeclaration")
+    @TestCaseName("add_with_delimiter_declaration('{0}')={1}")
+    public void test_that_string_calculator_support_different_delimiters(String numbersAsString, int expectedSum) {
+        StringCalculator calculator = new StringCalculator();
+
+        assertThat(calculator.add(numbersAsString)).isEqualTo(expectedSum);
+    }
+
+    private Object testDataWithDelimiterDeclaration() {
+        return new Object[][]{
+            /*"//[delimiter]\n[numbers…]"*/
+            { "//;\n1;2", 3 }
+        };
     }
 
 //    @Test
