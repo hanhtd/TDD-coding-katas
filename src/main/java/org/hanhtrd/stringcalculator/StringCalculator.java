98d42c3d0d224bcfbe92cf6164749ffafe7350dc
Hanh Truong <hanhtrd@yahoo.com>
2022-03-03 11:51:55 +0700

    String Calculator TDD kata + Finish step 4

--- src/main/java/org/hanhtrd/stringcalculator/StringCalculator.java
+++ src/main/java/org/hanhtrd/stringcalculator/StringCalculator.java
@@ -1,22 +1,45 @@
 package org.hanhtrd.stringcalculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 public class StringCalculator {
 
-    public static final String PARAMETER_SEPARATOR = ",";
+    public static final String COMMA_PARAMETER_SEPARATOR = ",";
+    public static final String NEW_LINE_PARAMETER_SEPARATOR = "\n";
+    public static final List<String> INVALID_PARTS = List.of(
+        ",\n",
+        "\n,",
+        ",,",
+        "\n\n"
+    );
 
-    public int add(String numbers) {
-        if (numbers.isEmpty()) {
+    public int add(String numbersAsString) {
+        validate(numbersAsString);
+
+        if (numbersAsString.isEmpty()) {
             return 0;
         }
-        if (!numbers.contains(PARAMETER_SEPARATOR)) {
-            return Integer.parseInt(numbers);
+
+        String[] firstSpitedItemArr = numbersAsString.split(COMMA_PARAMETER_SEPARATOR);
+        List<String> secondSpitedItemList = new ArrayList<>();
+        for (String parameter : firstSpitedItemArr) {
+            String[] numberItems = parameter.split(NEW_LINE_PARAMETER_SEPARATOR);
+            secondSpitedItemList.addAll(asList(numberItems));
         }
 
-        String[] parameters = numbers.split(PARAMETER_SEPARATOR);
         int sum = 0;
-        for (String parameter : parameters) {
-            sum += Integer.parseInt(parameter);
+        for (String numberItem : secondSpitedItemList) {
+            sum += Integer.parseInt(numberItem);
         }
         return sum;
+    }
+
+    private void validate(String numbersAsString) {
+        if (INVALID_PARTS.stream().anyMatch(numbersAsString::contains)) {
+            throw new IllegalArgumentException("Input value is invalid");
+        }
     }
 }
--- src/test/java/org/hanhtrd/stringcalculator/StringCalculatorUnitTest.java
+++ src/test/java/org/hanhtrd/stringcalculator/StringCalculatorUnitTest.java
@@ -1,44 +1,76 @@
 package org.hanhtrd.stringcalculator;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import junitparams.naming.TestCaseName;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameter;
-import org.junit.runners.Parameterized.Parameters;
 
 import java.util.Collection;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
-@RunWith(Parameterized.class)
+@RunWith(JUnitParamsRunner.class)
 public class StringCalculatorUnitTest {
 
-    @Parameters(name = "add({0})={1}")
-    public static Collection<Object[]> data() {
+    @Test
+    @Parameters(method = "testData")
+    @TestCaseName("add({0})={1}")
+    public void test_that_string_calculator_return_expected_total_value(String numbersAsString, int expectedSum) {
+        StringCalculator calculator = new StringCalculator();
+
+        assertThat(calculator.add(numbersAsString)).isEqualTo(expectedSum);
+    }
+
+    private Collection<Object[]> testData() {
         return List.of(
             new Object[][]{
-                { "",           0 },
-                { "1",          1 },
-                { "10",         10 },
-                { "21",         21 },
-                { "1,2",        3 },
-                { "6,4",        10 },
-                { "1,4,5",      10 }
+                { "", 0 },
+
+                { "1", 1 },
+
+                { "10", 10 },
+                { "21", 21 },
+
+                { "1,2", 3 },
+                { "6,4", 10 },
+                { "1,4,5", 10 },
+
+                { "1\n4\n5", 10 },
+                { "1\n4\n5\n7", 17 },
+
+                { "1,4\n5", 10 },
+                { "1\n4,5", 10 },
             }
         );
     }
 
-    @Parameter(0)
-    public String _numbersAsString;
-
-    @Parameter(1)
-    public int _expectedSum;
-
     @Test
-    public void test_that_string_calculator_works_as_expected() {
+    @Parameters(method = "erroneousData")
+    @TestCaseName("{0} is invalid input")
+    public void test_that_exception_thrown_for_invalid_input(String invalidInput) {
         StringCalculator calculator = new StringCalculator();
 
-        assertThat(calculator.add(_numbersAsString)).isEqualTo(_expectedSum);
+        assertThatThrownBy(() -> calculator.add(invalidInput))
+            .isExactlyInstanceOf(IllegalArgumentException.class)
+            .hasMessage("Input value is invalid");
     }
+
+    private List<String> erroneousData() {
+        return List.of(
+            "1,\n",
+            "1\n,",
+            "\n,1",
+            ",\n1",
+            "1,,",
+            "1\n\n\n"
+        );
+    }
+
+//    @Test
+//    public void string_splitting_test() {
+//        assertThat("\n".split("\n")).isEmpty();
+//        assertThat("abc".split("abc")).isEmpty();
+//    }
 }
